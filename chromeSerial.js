e4cf2646a4775e680fb7a871dcf8b4e65a94f725
LeChe27 <86964103+LeChe27@users.noreply.github.com>
2021-07-07 08:03:29 +0200

    Update chromeSerial.js

--- chromeSerial.js
+++ chromeSerial.js
@@ -1,12 +1,13 @@
 'use strict';
 
 console.log("TODO 'chromeSerial.js'");
+console.log(navigator.serial);
 
 if ("serial" in navigator) {
     console.log("serial")
     // The Web Serial API is supported.
     //const ports = await navigator.serial.getPorts();
-    navigator.serial.getPorts(function(devices) {
+    /*navigator.serial.getPorts(function(devices) {
         console.log(devices);
-    });
+    });*/
 }
